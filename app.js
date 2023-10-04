72da2677c59a44937c1d496b78b6c6d19bb665d0
emmyssj <sanjobamgbade@yahoo.com>
2023-10-04 16:27:55 +0100

    updated

--- app.js
+++ app.js
@@ -116,3 +116,39 @@ const viewResults = document.getElementById("view-results");
 viewResults.addEventListener("click", showResults);
 
 renderProducts();
+
+// function to create a new chart
+function renderChart() {
+  const ctx = document.getElementById("myChart");
+
+  const labels = [];
+  const views = [];
+  const clicks = [];
+
+  // loop through my products array and add in the label, views and clicks data to my arrays
+  for (let i = 0; i < products.length; i++) {
+    labels.push(products[i].name);
+    views.push(products[i].views);
+    clicks.push(products[i].clicks);
+  }
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "# of views",
+          data: views,
+          borderWidth: 1,
+        },
+        {
+          type: "line",
+          label: "# of clicks",
+          data: clicks,
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+}
